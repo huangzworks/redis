173b552de6810cd04ebca276add198cedbdb0583
huangz1990 <huangz1990@gmail.com>
2012-04-02 02:44:26 +0800

    readthedocs 无法 build ，选回默认 pyramid theme

--- conf.py
+++ conf.py
@@ -91,7 +91,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'pyramid_with_disqus'
+# html_theme = 'pyramid_with_disqus'
+html_theme = 'pyramid'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
