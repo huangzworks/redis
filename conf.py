92251a08e361cec35ca326cd6f1d59204e471f90
huangz1990 <huangz1990@gmail.com>
2015-08-01 09:02:33 +0800

    更新版权信息上的年份

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Redis 命令参考'
-copyright = u'2013, Redis'
+copyright = u'2015, Redis'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
